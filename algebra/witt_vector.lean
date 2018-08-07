bab2159067637bbac48f7ee5df2725fa2f081c02
Johan Commelin <johan@commelin.net>
2018-08-07 07:14:04 +0200

    Making use of the new map2

--- algebra/witt_vector.lean
+++ algebra/witt_vector.lean
@@ -78,112 +78,32 @@ end finsupp
 
 end ring_hom_commutes_with_stuff
 
-namespace mv_polynomial
+-- namespace mv_polynomial
 
-variables {σ : Type u} [decidable_eq σ]
-variables {R : Type v} [decidable_eq R] [comm_ring R]
-variables {S : Type w} [decidable_eq S] [comm_ring S]
+-- variables {σ : Type u} [decidable_eq σ]
+-- variables {R : Type v} [decidable_eq R] [comm_ring R]
+-- variables {S : Type w} [decidable_eq S] [comm_ring S]
 
-instance : comm_ring (mv_polynomial σ R) := finsupp.to_comm_ring
 
-instance C_is_ring_hom : is_ring_hom (C : R → mv_polynomial σ R) :=
-{ map_one := C_1,
-  map_add := λ x y, finsupp.single_add,
-  map_mul := λ x y, eq.symm $ C_mul_monomial }
+-- variables {τ : Type w} [decidable_eq τ]
 
-theorem map_monomial (i : R → S) [is_ring_hom i]
-  (x : σ →₀ ℕ) (r : R) : functorial i (monomial x r) = monomial x (i r) :=
-begin
-  simp [functorial,finsupp.map_range,monomial],
-  apply finsupp.ext,
-  intro x,
-  simp [finsupp.single_apply],
-  split_ifs,
-  { refl },
-  { apply is_ring_hom.map_zero }
-end
+-- theorem eval_assoc₁
+-- (f : mv_polynomial σ R) (g : σ → mv_polynomial τ R) (h : τ → R)
+-- : f.eval (λ n : σ, (g n).eval h) = ((map C f).eval g).eval h :=
+-- begin
+--   simp [eval],
+--   sorry
+-- end
 
-instance functorial_is_ring_hom (i : R → S) [is_ring_hom i] :
-is_ring_hom (functorial i : mv_polynomial σ R → mv_polynomial σ S) := sorry
--- { map_one :=
---   begin
---     dsimp [functorial, finsupp.map_range],
---     ext x,
---     dsimp [finsupp.on_finset_apply],
---     have H1 : (1 : mv_polynomial σ R) = (1 : (σ →₀ ℕ) →₀ R) := rfl,
---     have H2 : (1 : mv_polynomial σ S) = (1 : (σ →₀ ℕ) →₀ S) := rfl,
---     rw [H1, H2, finsupp.one_def, finsupp.one_def, finsupp.single_apply, finsupp.single_apply],
---     split_ifs,
---     { apply is_ring_hom.map_one i },
---     { apply is_ring_hom.map_zero i }
---   end,
---   map_add :=
---   begin
---     intros f g,
---     simp [functorial,finsupp.map_range,function.comp],
---     apply finsupp.ext,
---     intro x,
---     simp [*,finsupp.single_apply],
---     rw is_ring_hom.map_add i,
---   end,
---   map_mul :=
---   begin
---     intros f g,
---     dsimp [functorial, finsupp.map_range],
---     ext,
---     dsimp [finsupp.on_finset_apply, finsupp.mul_def],
---     rw [finsupp.sum_apply, finsupp.sum_apply, ring_hom_sum.finsupp i],
---     sorry,
---     { intros, simp,
---       sorry },
---     sorry, sorry, sorry
---   end }
+-- theorem eval_assoc₂
+-- (f : mv_polynomial σ (mv_polynomial τ R)) (g : σ → mv_polynomial τ R) (h : τ → R)
+-- : C ((f.eval g).eval h) = f.eval (λ n : σ, C ((g n).eval h)) :=
+-- begin
+--   simp [eval],
+--   sorry
+-- end
 
-lemma functorial_ring_hom_X (i : R → S) [is_ring_hom i] (n : σ)
- : functorial i (X n) = X n :=
-begin
-  simp [functorial,X,finsupp.map_range,function.comp,C,monomial,*],
-  apply finsupp.ext,
-  intro x,
-  simp [*,finsupp.single_apply],
-  split_ifs,
-  { apply is_ring_hom.map_one },
-  { apply is_ring_hom.map_zero }
-end
-
-lemma functorial_ring_hom_C (i : R → S) [is_ring_hom i] (r : R)
-: functorial i (C r) = (C (i r) : mv_polynomial σ S) :=
-begin
-  simp [functorial,X,finsupp.map_range,function.comp,C,monomial,*],
-  apply finsupp.ext,
-  intro x,
-  simp [*,finsupp.single_apply],
-  split_ifs,
-  { refl },
-  { apply is_ring_hom.map_zero }
-end
-
--- variables {σ : Type v} [decidable_eq σ]
--- variables {R : Type u} [decidable_eq R] [comm_ring R]
-variables {τ : Type w} [decidable_eq τ]
-
-theorem eval_assoc₁
-(f : mv_polynomial σ R) (g : σ → mv_polynomial τ R) (h : τ → R)
-: f.eval (λ n : σ, (g n).eval h) = ((functorial C f).eval g).eval h :=
-begin
-  simp [eval],
-  sorry
-end
-
-theorem eval_assoc₂
-(f : mv_polynomial σ (mv_polynomial τ R)) (g : σ → mv_polynomial τ R) (h : τ → R)
-: C ((f.eval g).eval h) = f.eval (λ n : σ, C ((g n).eval h)) :=
-begin
-  simp [eval],
-  sorry
-end
-
-end mv_polynomial
+-- end mv_polynomial
 
 namespace nat
 
@@ -222,49 +142,54 @@ lemma X_in_terms_of_W_eq {n : ℕ} : X_in_terms_of_W n =
 := X_in_terms_of_W.equations._eqn_1 n
 
 -- This proof goes a long way... but deterministic timeouts keep bugging me. So I sorried it for the moment.
-lemma X_in_terms_of_W_prop (n : ℕ) : (functorial C (X_in_terms_of_W n)).eval witt_polynomial = X n :=
+lemma X_in_terms_of_W_prop (n : ℕ) : (X_in_terms_of_W n).map₂ witt_polynomial C = X n :=
 begin
   apply nat.strong_induction_on n,
   intros n H,
   rw X_in_terms_of_W_eq,
-  rw is_ring_hom.map_mul (functorial C),
-  { rw is_ring_hom.map_sub (functorial C),
-    { rw functorial_ring_hom_X,
+  rw map₂_mul,
+  rw sub_eq_add_neg,
+  rw map₂_add,
+  rw map₂_X,
+  rw map₂_C,
+  rw is_ring_hom.map_mul (map C),
+  { rw is_ring_hom.map_sub (map C),
+    { rw map_ring_hom_X,
       rw sub_mul,
       rw sub_eq_add_neg,
       rw eval_add,
       rw eval_mul,
       rw eval_X,
-      rw functorial_ring_hom_C,
+      rw map_ring_hom_C,
       rw eval_C,
       rw is_ring_hom.map_neg (eval witt_polynomial),
       rw is_ring_hom.map_mul (eval witt_polynomial),
       rw eval_C,
       rw neg_mul_eq_neg_mul,
       suffices : witt_polynomial n + -eval witt_polynomial
-            (functorial C
+            (map C
                (finset.sum finset.univ (λ (i : fin n), ↑p ^ i.val * X_in_terms_of_W (i.val) ^ p ^ (n - i.val)))) =
         X n * C (↑p ^ (n + 1)),
       { sorry },
       { rw show eval witt_polynomial
-          (functorial C
+          (map C
              (finset.sum finset.univ (λ (i : fin n), ↑p ^ i.val * X_in_terms_of_W (i.val) ^ p ^ (n - i.val)))) =
              
              finset.sum finset.univ (λ (i : fin n), ↑p ^ i.val * (eval witt_polynomial
-          ((functorial C) (X_in_terms_of_W (i.val))) ^ p ^ (n - i.val))),
+          ((map C) (X_in_terms_of_W (i.val))) ^ p ^ (n - i.val))),
         { sorry },
         rw show (λ (i : fin n),
-             ↑p ^ i.val * eval witt_polynomial (functorial C (X_in_terms_of_W (i.val))) ^ p ^ (n - i.val)) = (λ (i : fin n),
+             ↑p ^ i.val * eval witt_polynomial (map C (X_in_terms_of_W (i.val))) ^ p ^ (n - i.val)) = (λ (i : fin n),
              ↑p ^ i.val * (X (i.val)) ^ p ^ (n - i.val)),
         { funext i,
           rw (H i.1 i.2) },
         -- unfold witt_polynomial,
         sorry },
-      -- rw [ring_hom_sum.finset (functorial C)],
-      -- rw ring_hom_sum (eval witt_polynomial) finset.univ (λ (x : fin n), functorial C (↑p ^ x.val * X_in_terms_of_W (x.val) ^ p ^ (n - x.val))),
+      -- rw [ring_hom_sum.finset (map C)],
+      -- rw ring_hom_sum (eval witt_polynomial) finset.univ (λ (x : fin n), map C (↑p ^ x.val * X_in_terms_of_W (x.val) ^ p ^ (n - x.val))),
       sorry },
-    { exact mv_polynomial.functorial_is_ring_hom C } },
-  { exact mv_polynomial.functorial_is_ring_hom C }
+    { exact mv_polynomial.map_is_ring_hom C } },
+  { exact mv_polynomial.map_is_ring_hom C }
 end
 
 -- theorem X_in_terms_of_W_prop2 (n : ℕ) : (witt_polynomial n).eval (X_in_terms_of_W) = X n :=
@@ -276,7 +201,7 @@ end
 -- end
 
 -- theorem X_in_terms_of_W_prop3 (f g : ℕ → mv_polynomial (bool × ℕ) ℚ) :
--- (∀ n, (functorial C (X_in_terms_of_W n)).eval f = (functorial C (X_in_terms_of_W n)).eval g) → f = g :=
+-- (∀ n, (map C (X_in_terms_of_W n)).eval f = (map C (X_in_terms_of_W n)).eval g) → f = g :=
 -- begin
 --   intro H,
 --   funext n,
@@ -284,18 +209,18 @@ end
 -- end
 
 -- def witt_structure_rat (Φ : mv_polynomial bool ℚ) (n : ℕ) : mv_polynomial (bool × ℕ) ℚ :=
--- (functorial C (X_in_terms_of_W n)).eval (λ k : ℕ,
---    (functorial C Φ).eval (λ b, ((witt_polynomial k).eval (λ i, X (b,i))))
+-- (map C (X_in_terms_of_W n)).eval (λ k : ℕ,
+--    (map C Φ).eval (λ b, ((witt_polynomial k).eval (λ i, X (b,i))))
 -- )
 
 -- lemma witt_structure_rat_eq (Φ : mv_polynomial bool ℚ) (n : ℕ) :
--- witt_structure_rat Φ n = (functorial C (X_in_terms_of_W n)).eval (λ k : ℕ,
---    (functorial C Φ).eval (λ b, ((witt_polynomial k).eval (λ i, X (b,i))))
+-- witt_structure_rat Φ n = (map C (X_in_terms_of_W n)).eval (λ k : ℕ,
+--    (map C Φ).eval (λ b, ((witt_polynomial k).eval (λ i, X (b,i))))
 -- ) := rfl
 
 
 -- theorem witt_structure_prop (Φ : mv_polynomial bool ℚ) (φ : ℕ → mv_polynomial (bool × ℕ) ℚ) :
--- (∀ n : ℕ, (witt_polynomial n).eval φ = (functorial C Φ).eval (λ b, ((witt_polynomial n).eval (λ i, X (b,i)))))
+-- (∀ n : ℕ, (witt_polynomial n).eval φ = (map C Φ).eval (λ b, ((witt_polynomial n).eval (λ i, X (b,i)))))
 -- ↔ φ = witt_structure_rat Φ
 -- :=
 -- begin
@@ -303,13 +228,13 @@ end
 --   { intro H,
 --     funext n,
 --     unfold witt_structure_rat,
---     rw show (λ (k : ℕ), eval (λ (b : bool), eval (λ (i : ℕ), X (b, i)) (witt_polynomial k)) (functorial C Φ)) =
+--     rw show (λ (k : ℕ), eval (λ (b : bool), eval (λ (i : ℕ), X (b, i)) (witt_polynomial k)) (map C Φ)) =
 --     (λ (k : ℕ), eval φ (witt_polynomial k)),
 --     { funext k,
 --       exact (H k).symm },
---     rw show φ n = (functorial C ((functorial C (X_in_terms_of_W n)).eval witt_polynomial)).eval φ,
+--     rw show φ n = (map C ((map C (X_in_terms_of_W n)).eval witt_polynomial)).eval φ,
 --     { rw X_in_terms_of_W_prop,
---       rw functorial_ring_hom_X C,
+--       rw map_ring_hom_X C,
 --       exact eval_X.symm },
 --     simp,
 --     sorry },
